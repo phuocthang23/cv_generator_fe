b58e284ec8a0711f289b31b110f6fff15ba25b50
ThangNP <phuocthang238@gmail.com>
2024-03-13 15:45:50 +0700

    layout

--- src/page/User/UserLayout.tsx
+++ src/page/User/UserLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "../../layouts/header/header";
-import Footer from "../../layouts/footer/footer";
+import Header from "../../layouts/header";
+import Footer from "../../layouts/footer";
 
 const UserLayout = () => {
   return (
